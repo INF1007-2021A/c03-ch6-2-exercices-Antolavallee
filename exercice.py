4db370b2f7d9fe54f98b7898215c61f1b02fb631
Antolavallee <antoine.lavallee@polymtl.ca>
2021-10-04 13:59:59 -0400

    Exo sup 6

--- exercice.py
+++ exercice.py
@@ -6,26 +6,39 @@ from matplotlib.colors import cnames
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
-
-    return {}
-
+    dict_new = {}
+    for items in some_list:
+        dict_new[items] = some_list.index(items)
+    return dict_new
+    
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-
-    return []
+    list_colors = []
+    for names in colors:
+        hex = cnames.get(names)
+        list_colors.append((names, hex))
+    return list_colors
 
 
 def create_list() -> list:
     # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
-
-    return []
+    list_nbr = []
+    for numbers in range(10000):
+        if not (numbers >= 15 and numbers <= 350):
+            list_nbr.append(numbers)
+    return list_nbr
 
 
 def compute_mse(model_dict: dict) -> dict:
     # TODO: Calculer l'erreur quadratique moyen pour chaque modèle. Retourner un dictionnaire contenant les MSE.
-
-    return {}
+    dict_new = {}
+    for key, value in model_dict.items():
+        sum_err = 0
+        for val in value: 
+            sum_err += (val[0] - val[1])**2
+            dict_new.update({key: sum_err/len(value)})
+    return dict_new
 
 
 def main() -> None:
